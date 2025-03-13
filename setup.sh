390e082fb7251e3d28297052f9984ee87fac5b3a
Carter Perez <cartertechsolution@gmail.com>
2025-03-13 00:54:59 -0400

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+# Ubuntu development environment setup script
+
+# Update package lists
+apt update
+apt upgrade -y
+
+# Install essential tools
+apt install -y git curl wget nano build-essential
+
+# Install Docker
+apt install -y apt-transport-https ca-certificates gnupg lsb-release
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
+apt update
+apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin
+
+# Install Docker Compose
+curl -SL https://github.com/docker/compose/releases/download/v2.23.0/docker-compose-linux-x86_64 -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install ZSH
+apt install -y zsh
+
+# Install Oh My ZSH
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+
+# Install useful ZSH plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-completions
+
+# Configure ZSH with plugins
+sed -i 's/plugins=(git)/plugins=(git docker docker-compose zsh-syntax-highlighting zsh-autosuggestions zsh-completions)/' ~/.zshrc
+echo 'autoload -U compinit && compinit' >> ~/.zshrc
+
+# Set ZSH as default shell
+chsh -s $(which zsh)
+
+# Install a nice theme (Powerlevel10k - very popular)
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel10k\/powerlevel10k"/' ~/.zshrc
+
+echo "Setup complete! Please log out and log back in, or run 'zsh' to start using your new shell."
